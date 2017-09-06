6e0aa272fa8c02c5a096b1b5872d9278817da9e1
ljsherlock <lewis@homesick.co>
2017-09-06 10:22:54 +0100

    Moved all sidebar code from Page to Single because both may have Widgets and Page extends Single.

--- site/web/app/themes/ljsherlock/MVC/Models/Page.php
+++ site/web/app/themes/ljsherlock/MVC/Models/Page.php
@@ -2,41 +2,17 @@
 
 namespace MVC\Models;
 
+// use Includes\Classes\CMB2 as CMB2;
+
 class Page extends Single
 {
-    public $sidebars = array();
-
     public function __construct($args)
     {
         parent::__construct($args);
-
-        $this->sidebars = array( 'sidebar', 'sidebar__header', 'sidebar__footer', 'sidebar__homepage_main', 'sidebar__after_app' );
-
-        if( isset( $this->args['sidebars'] ) )
-        {
-            array_merge( $this->sidebars, $this->args['sidebars'] ) ;
-        }
     }
 
     public function get()
     {
-        $this->timber->addContext( array(
-            'menu' => new \TimberMenu('Primary'),
-        ) );
-
-        if( isset($this->sidebars) )
-        {
-            $this->addSidebar( $this->sidebars );
-        }
-
         return parent::get();
-    }
-
-    public function addSidebar($sidebars)
-    {
-        foreach ($sidebars as $key => $sidebar)
-        {
-            $this->timber->addContext( array( $sidebar => \Timber::get_widgets( $sidebar ) ) );
-        }
     }
 }
--- site/web/app/themes/ljsherlock/MVC/Models/Single.php
+++ site/web/app/themes/ljsherlock/MVC/Models/Single.php
@@ -2,8 +2,12 @@
 
 namespace MVC\Models;
 
+use Includes\Classes\CMB2 as CMB2;
+
 class Single extends Base
 {
+
+    public $sidebars = array();
 
     /**
     *   @method __construct
@@ -12,6 +16,21 @@ class Single extends Base
     public function __construct($args)
     {
         parent::__construct($args);
+
+        // set Sidebars and merge
+        $this->sidebars = array(
+            'sidebar',
+            'sidebar__header',
+            'sidebar__footer',
+            'sidebar__homepage_main',
+            'sidebar__after_app',
+        );
+        if( isset( $this->args['sidebars'] ) )
+        {
+            array_merge( $this->sidebars, $this->args['sidebars'] ) ;
+        }
+
+        $this->post = new \TimberPost();
     }
 
     /**
@@ -22,14 +41,41 @@ class Single extends Base
     **/
     public function get()
     {
-        $post = new \TimberPost();
+        // if sidebars exist, call addSidebar().
+        if( isset($this->sidebars) )
+        {
+            $this->addSidebar( $this->sidebars );
+        }
+
+        // query Work posts for Footer
+        $archive = new \MVC\Models\Archive(array());
+        $args = array( 'query' => array( 'posts_per_page' => 3 ) );
+        $posts = $archive->query($args);
 
         $this->timber->addContext( array(
-            'post' => $post,
-            'terms' => $this->terms($post),
+            // content
+            'subtitle' => get_post_meta( $this->post->ID, CMB2::$prefix . 'subtitle', true ),
+            'post' => $this->post,
+            //header
+            'menu' => new \TimberMenu('Primary'),
+            // footer
+            'email' => CMB2::myprefix_get_option( CMB2::$prefix . 'contact_email'),
+            'telephone' => CMB2::myprefix_get_option(CMB2::$prefix . 'contact_telephone'),
+            'social_media' => CMB2::myprefix_get_option( CMB2::$prefix . 'social_media_links'),
+            'footer_posts' => $posts,
+            'footer_menu' => new \TimberMenu('Footer'),
+            'copyright' => 'Copyright ' . Date('Y') . ' Sherlock Ltd',
         ));
 
         return parent::get();
+    }
+
+    public function addSidebar($sidebars)
+    {
+        foreach ($sidebars as $key => $sidebar)
+        {
+            $this->timber->addContext( array( $sidebar => \Timber::get_widgets( $sidebar ) ) );
+        }
     }
 
     public function terms($post)
