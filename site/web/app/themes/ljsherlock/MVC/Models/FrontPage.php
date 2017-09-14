1d5cdf747b25293b762171965c7f40483a7eb97a
ljsherlock <lewis@homesick.co>
2017-09-14 01:02:48 +0100

    Minor changes to CMB2 call and changes in variable names.

--- site/web/app/themes/ljsherlock/MVC/Models/FrontPage.php
+++ site/web/app/themes/ljsherlock/MVC/Models/FrontPage.php
@@ -18,17 +18,16 @@ class Frontpage extends Page
     public function get()
     {
         // Work Posts: most recent.
-        $archive = new \MVC\Models\Archive(array());
-        $args = array( 'post_type' => 'work', 'query' => array( 'posts_per_page' => 1 ) );
-        $work = $archive->query($args)[0];
+        $work = new \MVC\Controllers\Archive__Work( array( 'post_type' => 'work', 'query' => array('posts_per_page' => 1 ) ) );
+        $workData = $work->model->get();
 
         $this->timber->addContext( array(
-            'work' => array('post' => $work, 'terms' => $archive->get_hierachical_terms($work, 'stats')['overview'] ),
-            'captions ' => array(
-                // 'instagram' => $this->instagram(),
+            'work' => $workData['posts'],
+            'captions' => array(
                 'left' => get_post_meta( $this->post->ID, CMB2::$prefix . 'hero_caption_left', true ),
                 'right' => get_post_meta( $this->post->ID, CMB2::$prefix . 'hero_caption_right', true )
             ),
+            'instagram' => $this->instagram(),
         ) );
 
         return parent::get();
@@ -36,19 +35,20 @@ class Frontpage extends Page
 
     private function instagram()
     {
-        $access_token = CMB2::myprefix_get_option( 'instagram_at' );
-
+        $access_token = CMB2::myprefix_get_option( CMB2::$prefix . 'instagram_at' );
         $recents = json_decode( file_get_contents('https://api.instagram.com/v1/users/self/media/recent/?access_token='. $access_token) );
         $images = array();
 
-        foreach ($recents->data as $key => $item)
+        if($recents->data)
         {
-            # code...
-            $image = json_decode( file_get_contents('https://api.instagram.com/v1/media/'.$item->id.'?access_token='. $access_token) );
-            array_push($images, array( 'url' => $image->data->images->standard_resolution->url, 'caption' => $item->caption->text ));
+            foreach ($recents->data as $key => $item)
+            {
+                $caption = $item->caption->text ?? null;
+                $image = $item->images->standard_resolution->url ?? null;
+                array_push($images, array( 'url' => $image, 'caption' => $caption ));
+            }
+            return $images;
         }
-
-        return $images;
     }
 
 }
