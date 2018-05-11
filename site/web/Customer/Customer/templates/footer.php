e0b36512ebfdce62185f8b9e9a64468d38609be0
ljsherlock <lewis@ljsherlock.com>
2018-05-11 11:36:18 +0700

    feat: add all simple icons, remove sharpend

--- site/web/Customer/Customer/templates/footer.php
+++
@@ -1,7 +0,0 @@
-    </main>
-
-	<!--Import jQuery before materialize.js-->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="js/theme.min.js"></script>
-</body>
-</html>
--- site/web/Platform/index.php
+++
@@ -1,5 +0,0 @@
-<?php require('templates/header.php'); ?>
-
-<?php require('pages/clients_list.php'); ?>
-
-<?php require('templates/footer.php'); ?>
